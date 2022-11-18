bfa1a517c98d2c031b4e52e2e223955b2da78661
d1vya29 <divyapate29@gmail.com>
2022-11-18 10:54:54 +0000

    buttons

--- src/components/Contact-us.js
+++ src/components/Contact-us.js
@@ -1,21 +1,17 @@
 import "./Contact-us.css";
 export default function Contactus(props){
     return(
+
         <div className="Contact-us">
-            <h1> Contact Us</h1>
-            <div className="name">
-                <input type= "text" id="input" placeholder="Enter Your Name"></input>
-                <input type= "text" id="input" placeholder="Enter Your Surname"></input>
-            </div>
-            <input type= "text" id="input" placeholder="Enter Your Email"></input>
-            <input type="submit" id ="btn"></input>
-
-
-
-
-
-
-
+            <form className="details">
+                <div className="name">
+                    <label>first name<input type= "text" ></input></label>
+                    <label>surname<input type= "text"  ></input></label>
+                </div>
+                <label className="email">email<input type= "text" ></input></label>
+                <label className="help">what can we help you with?<input type= "text"  ></input></label>
+                <input type="submit" id ="btn"></input>
+            </form>
         </div>
     )
 }
