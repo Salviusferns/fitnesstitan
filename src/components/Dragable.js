2ad1b1131c2d15324b35a1d3269b096ccdec04bf
Salviusferns <40175316@hcuc.ac.uk>
2022-11-18 16:55:01 +0000

    drop

--- src/components/Dragable.js
+++ src/components/Dragable.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import './Dragable.css';
 import Draggable from 'react-draggable';
-export default function Dragable(props){
+export default function Dragable(props){;
+    const [x, setX]= useState(0)
+    const [y, setY]= useState(0)
+    const handleStop = (event, dragElement) => {
+    setX(dragElement.x)
+    setY(dragElement.y)
+  };
 
-    return(
+    return (
         <div>
             <section>
-                <Draggable>
+                <Draggable >
                  <div className="box">
                   <button>COMPONENT</button>
                  </div>
