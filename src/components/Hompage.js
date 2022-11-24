9d9b6f184cf0d8625eb6139c6ee4c086f1286387
blackadamlol <ssahilpreet35@gmail.com>
2022-11-24 16:56:20 +0000

    made #

--- src/components/Hompage.js
+++ src/components/Hompage.js
@@ -8,10 +8,10 @@ export default function Homepage(props){
                 <header>Go Hard Or Go Home!!</header>
                 <p className="p1">Never give up on a dream just because of the time it will take to accomplish it. The time will pass anyway.
                 So dont waste your time and <strong><a href="/Membership">JOIN NOW</a></strong></p>
-                <button className="btnmore">More About Gym</button>
+                <a href="#hi"><button className="btnmore" >More About Gym</button></a>
                 <h2 className="h1">Why Should You Join GYM?</h2>
                 <p className="p2">Joining a gym can help you stay motivated to exercise consistently. This is a great way to build muscle, lose weight, lower blood pressure, boost mental focus, and more. Over time, you can look better, feel better, and accomplish things you never thought possible!</p>
-                <h1 className="h2">More About Us</h1>
+                <h1 className="h2" id="hi">More About Us</h1>
                 <img className="girlpic" src={girl} alt="Girl Training with dumbles"/>
             </div>
             
--- src/components/Membership.js
+++ src/components/Membership.js
@@ -1,5 +1,19 @@
+import "./membership.css"
 export default function Membership(props){
     return(
-        <h1>hi</h1>
+        <div className="table">
+            <ul className="list">
+                
+                <li className="li1">
+                    <h2>Silver</h2>
+                    <p>£17.99/pm per person</p>
+                    <hr></hr>
+                </li>
+              
+                <li><h2>Gold</h2></li>
+                <li><h2>Platnuim</h2></li>
+            </ul>
+            <div className="black"></div>
+        </div>
     )
 }
