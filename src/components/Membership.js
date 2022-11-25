09374655a9170f323d61cf8a1f7ad8e1dbe4856a
blackadamlol <ssahilpreet35@gmail.com>
2022-11-25 15:59:45 +0000

    membership

--- src/components/Membership.js
+++ src/components/Membership.js
@@ -15,6 +15,7 @@ export default function Membership(props){
                     <p><s>Access to Meal Planing</s></p>
                     <p><s>Access to Personalised Videos</s></p>
                     <p><s>Fitness/body composition tracking</s></p>
+                    <p><s>Access to All National-Wide Gyms</s></p>
                     <div className="btn-select">
                         <a href="/Homepage">
                             <li>Select</li>
@@ -22,9 +23,9 @@ export default function Membership(props){
                     </div>
                 </li>
               
-                <li className="li2"><h2>Gold</h2>
-                
-                <p>£24.99/pm per person</p>
+                <li className="li2">
+                    <h2>Gold</h2>
+                    <p>£24.99/pm per person</p>
                     <hr></hr>
                     <p>Access to Gym</p>
                     <p>Access to Free Training Videos</p>
@@ -33,13 +34,32 @@ export default function Membership(props){
                     <p><s>Access to Meal Planing</s></p>
                     <p><s>Access to Personalised Videos</s></p>
                     <p><s>Fitness/body composition tracking</s></p>
+                    <p><s>Access to All National-Wide Gyms</s></p>
                     <div className="btn-select">
                         <a href="/Homepage">
                             <li>Select</li>
                         </a>
-                    </div></li>
+                    </div>
+                </li>
 
-                <li><h2>Platnuim</h2></li>
+                <li className="li3">
+                    <h2>Platnuim</h2>
+                    <p>£35.99/pm per person</p>
+                    <hr></hr>
+                    <p>Access to Gym</p>
+                    <p>Access to Free Training Videos</p>
+                    <p>Free Gym Classes</p>
+                    <p> Bring A Friend</p>
+                    <p>Access to Meal Planing</p>
+                    <p>Access to Personalised Videos</p>
+                    <p>Fitness/body composition tracking</p>
+                    <p>Access to All National-Wide Gyms</p>
+                    <div className="btn-select">
+                        <a href="/Homepage">
+                            <li>Select</li>
+                        </a>
+                    </div>
+                </li>
             </ul>
             <div className="black"></div>
         </div>
