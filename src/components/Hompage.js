55ef3c9b52df66911b9ad0149ee1ee7a091a234d
blackadamlol <ssahilpreet35@gmail.com>
2022-12-02 10:52:16 +0000

    pop up done

--- src/components/Hompage.js
+++ src/components/Hompage.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 
 export default function Homepage(props){
     const navigate = useNavigate();
+    const [buttonPopup, setButtonPopup] = useState(false);
 
     return(
             <div className="info">
@@ -13,30 +14,49 @@ export default function Homepage(props){
                             <header>
                                 Go Hard Or Go Home!!
                             </header>
+
                             <p className="p1">
                                 Never give up on a dream just because of the time it will take to accomplish it. The time will pass anyway.
                                 So dont waste your time and <strong><a onClick={() => {navigate("/membership")}}>JOIN NOW</a></strong>
-                            </p>
+                            </p> 
                         </div>
+
                             <button className="btnmore"  onClick={() => {navigate("#more")}}>More About Gym</button>
+                            
                         <div className="morepage">
                             <h1>More About Us</h1>
-                            <div className="galery1">
-                                <div className="morepage-1">
-                                    <div className="text">
-                                        <h2><strong> Gym Rules For Using Equipments</strong></h2>
-                                        <ul>
-                                            <li>
-                                                Put your weights back<button>More</button>
-                                                <Popups trigger={true}>
-                                                    <h3>hi</h3>
-                                                </Popups>
-                                            </li>
-                                            <li>Keep the equipment where it belongs</li>
-                                            <li>Be mindful of your surroundings</li>
-                                        </ul>
+                                <div className="galery1">
+                                    <div className="morepage-1">
+                                        <div className="text">
+                                            <h2><strong> Gym Rules For Using Equipments</strong></h2>
+                                            <ul>
+                                                <li>
+                                                    <p>Put your weights back</p> <button onClick={() => setButtonPopup(true)}>More</button>
+                                                    <Popups trigger={buttonPopup} setTrigger = {setButtonPopup}>
+                                                        <h3>hi</h3>
+                                                    </Popups>
+                                                </li>
+                                                <li>
+                                                    <p>Keep the equipment where it belongs</p> <button onClick={() => setButtonPopup(true)}>More</button>
+                                                    <Popups trigger={buttonPopup} setTrigger = {setButtonPopup}>
+                                                        <h3>hi</h3>
+                                                    </Popups>
+                                                </li>
+                                                <li>
+                                                    <p>Be mindful of your surroundings</p> <button onClick={() => setButtonPopup(true)}>More</button>
+                                                    <Popups trigger={buttonPopup} setTrigger = {setButtonPopup}>
+                                                        <h3>hi</h3>
+                                                    </Popups>
+                                                </li>
+                                                <li>
+                                                    <p>Be careful with your personal items</p> <button onClick={() => setButtonPopup(true)}>More</button>
+                                                    <Popups trigger={buttonPopup} setTrigger = {setButtonPopup}>
+                                                        <h3>hi</h3>
+                                                    </Popups>
+                                                </li>
+                                            </ul>
+                                        </div>
                                     </div>
-                                </div>
                                 <div className="morepage-2">
                                     <div className="text">
                                         <h2>Using equipments</h2>
--- src/components/Popups.js
+++ src/components/Popups.js
@@ -3,7 +3,7 @@ export default function Popups(props){
     return(props.trigger) ? (
         <div className="popup">
             <div className="popup-inner">
-                <button className="close-btn">close</button>
+                <button className="close-btn" onClick={() => props.setTrigger(false)}>close</button>
                 { props.children }
             </div>
         </div>
